fd4a4626a2e53275c9f0a9b00421740df5ea4f9d
Grace Frank <frankgracey@gmail.com>
2018-12-20 02:49:15 +0100

    added toUpper function to the String prototye

--- stringPrototype.js
+++ stringPrototype.js
@@ -2,3 +2,18 @@
 String.prototype.hasVowel = function hasVowel() {
     return /[aeiou]/.test(this);
 }
+
+//toUpper
+String.prototype.toUpper = function () {
+    let capitalizedString = '';
+
+    for (let i = 0; i < this.length; i++) {
+
+        if (/[a-z]/.test(this[i])) {
+            capitalizedString += String.fromCharCode(this.charCodeAt(i) - 32);
+        } else {
+            capitalizedString += this[i];
+        }
+    }
+    return capitalizedString;
+}
