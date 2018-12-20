02b74d979f74c1f08bf50e315791342a5f42c3e6
Grace Frank <frankgracey@gmail.com>
2018-12-20 13:06:09 +0100

    modified the test cases for has vowel and toUpper

--- stringPrototype.test.js
+++ stringPrototype.test.js
@@ -9,31 +9,31 @@ test('that hasVowel returns boolean value', () => {
   expect(typeof ('word'.hasVowel())).toBe('boolean');
 });
 
-test('that hasVowel returns boolean value', () => {
+test('that "wHEord".hasVowel returns true', () => {
   expect('wHEord'.hasVowel()).toBe(true);
 });
 
-test('that hasVowel returns boolean value', () => {
+test('that "Sql sw".hasVowel returns false', () => {
   expect('Sql sw'.hasVowel()).toBe(false);
 });
 
-test('that hasVowel returns boolean value', () => {
+test('that "wh_*hi*".hasVowel returns true value', () => {
   expect('wh_*hi*'.hasVowel()).toBe(true);
 });
 
-test('that hasVowel returns boolean value', () => {
+test('that "--$55".hasVowel does not return true', () => {
   expect('--$55'.hasVowel()).not.toBe(true);
 });
 
-test('that hasVowel returns boolean value', () => {
+test('that "6670rejhh".hasVowel returns  true', () => {
   expect('6670rejhh'.hasVowel()).toBe(true);
 });
 
-test('that hasVowel returns boolean value', () => {
+test('that "786540000".hasVowel returns false', () => {
   expect('786540000'.hasVowel()).toBe(false);
 });
 
-test('that hasVowel returns boolean value', () => {
+test('that "HEROD".hasVowel returns true', () => {
   expect('HEROD'.hasVowel()).toBe(true);
 });
 
@@ -45,6 +45,40 @@ test('that string has a method toUpper ', () => {
 
 test('that toUpper Method returns string type', () => {
   expect(typeof ('word'.toUpper())).toBe('string');
+});
+
+test('that "HELP".toUpper Method returns  "HELP"', () => {
+  expect('HELP'.toUpper()).toBe('HELP');
+});
+
+test('that "SoLace".toUpper Method returns "SOLACE"', () => {
+  expect('SoLace'.toUpper()).toBe('SOLACE');
+});
+
+test('that "55scale".toUpper Method returns "55SCALE"', () => {
+  expect('55scale'.toUpper()).toBe('55SCALE');
+});
+
+test('that "sql-server`".toUpper Method returs "SQL-SERVER`"', () => {
+  expect('sql-server`'.toUpper()).toBe('SQL-SERVER`');
+});
+
+test('that "3e + 3e = 6e".toUpper Method returns "3E + 3E = 6E"', () => {
+  expect('3e + 3e = 6e'.toUpper()).toBe('3E + 3E = 6E');
+});
+
+test('that "3*3".toUpper Method returns "3*3"', () => {
+  expect('3*3'.toUpper()).toBe('3*3');
+});
+
+
+//toLower
+test('that string has a method toUpper ', () => {
+  expect('toLower' in String.prototype).toBe(true);
+});
+
+test('that toUpper Method returns string type', () => {
+  expect(typeof ('word'.toLower())).toBe('string');
 });
 
 test('that toUpper Method returns string type', () => {
